6dace84ce1473919604b2f7aecb1eafd0680d5bc
tomik23 <gtomik@gmail.com>
2018-08-08 00:34:46 +0200

    conver css to scss

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,18 @@ const gulp          = require('gulp');
 const babel         = require('gulp-babel');
 const concat        = require('gulp-concat');
 const cssmin        = require('gulp-cssmin');
+const sass          = require('gulp-sass');
+const autoprefixer  = require('gulp-autoprefixer');
 const browserSync   = require('browser-sync');
 
 function swallowError(error) {
     console.log(error.toString())
     this.emit('end')
 }
+
+let autoprefixerOptions = {
+    browsers: ['> 1%', 'last 3 versions']
+};
 
 /**
  * js
@@ -23,6 +29,17 @@ gulp.task('js', function () {
 });
 
 /**
+ * sass
+ */
+gulp.task('scss', function () {
+    return gulp.src('sources/scss/**/*')
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(cssmin())
+        .pipe(autoprefixer(autoprefixerOptions))
+        .pipe(gulp.dest('./build'));
+});
+
+/**
  * html
  */
 gulp.task('html', function () {
@@ -31,21 +48,12 @@ gulp.task('html', function () {
 });
 
 /**
- * css
- */
-gulp.task('css', function () {
-    return gulp.src('sources/css/**/*')
-        .pipe(cssmin())
-        .pipe(gulp.dest('./build'));
-});
-
-/**
  * watch
  */
 gulp.task('watch', function () {
-    gulp.watch('sources/index.html', ['html', 'css', 'js']).on('change', browserSync.reload);
-    gulp.watch('sources/css/**/*.css', ['css']).on('change', browserSync.reload);
+    gulp.watch('sources/index.html', ['html', 'scss', 'js']).on('change', browserSync.reload);
+    gulp.watch('sources/scss/**/*.scss', ['scss']).on('change', browserSync.reload);
     gulp.watch('sources/js/**/*.js', ['js']).on('change', browserSync.reload);
 });
 
-gulp.task('default', ['watch', 'html', 'css', 'js']);
+gulp.task('default', ['watch', 'html', 'scss', 'js']);
