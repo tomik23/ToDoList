ca661c1c24f5518fd9bf5d797c4ba2ebfcfc8cbd
Grzegorz Tomicki <gtomicki@gmail.com>
2019-08-03 18:24:33 +0200

    remove postcss-url

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   plugins: {
     autoprefixer: {},
-    'postcss-url': {
-      url: 'inline',
-      maxSize: 50,
-    },
     'postcss-css-variables': {},
     cssnano: {
       safe: true,
